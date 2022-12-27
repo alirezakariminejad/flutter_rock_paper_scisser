a71137055f06dd5711c9f2351153ecdb2e0f4ba1
Alireza <alireza.kariminejad@yahoo.com>
2022-12-27 14:20:44 +0330

    Design game view

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,40 @@ class Application extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'vb'),
+      home: Scaffold(
+        backgroundColor: Colors.deepOrange[600],
+        appBar: AppBar(
+          title: Text('سنگ   کاغذ   قیچی'),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange,
+          elevation: 3.0,
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // SizedBox(width: double.infinity),
+                Image(image: AssetImage('images/1.png')),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'شروع بازی',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    )),
+                Image(image: AssetImage('images/2.png')),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
